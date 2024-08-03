a1c027cfc9290ca7da81f1138a2137a3b92b996b
Akashkhatri-cs <85150899+Akashkhatri-cs@users.noreply.github.com>
2024-08-03 00:41:38 -0400

    Create proxy.js

---
+++ api/proxy.js
@@ -0,0 +1,43 @@
+// pages/api/proxy.js
+import { createProxyMiddleware } from 'http-proxy-middleware';
+
+const apiProxy = createProxyMiddleware({
+    target: 'http://44.203.216.52', // Replace this with your AWS ECS backend IP or URL
+    changeOrigin: true,
+    pathRewrite: {
+        '^/api/proxy': '', // This removes '/api/proxy' from the request URL
+    },
+    onProxyReq: (proxyReq, req, res) => {
+        if (req.method === 'POST' && req.body) {
+            const bodyData = JSON.stringify(req.body);
+            // Ensure the correct Content-Type header is set
+            proxyReq.setHeader('Content-Type', 'application/json');
+            proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
+            // Write out body changes to the proxyReq stream
+            proxyReq.write(bodyData);
+        }
+    },
+});
+
+export default function handler(req, res) {
+    console.log('req',req)
+    console.log('res',res)
+    if (req.method === 'POST') {
+        return apiProxy(req, res, (result) => {
+            if (result instanceof Error) {
+                throw result;
+            }
+            return result;
+        });
+    } else if (req.method === 'GET') {
+        return apiProxy(req, res, (result) => {
+            if (result instanceof Error) {
+                throw result;
+            }
+            return result;
+        });
+    } else {
+        res.setHeader('Allow', ['GET', 'POST']);
+        res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
+}
